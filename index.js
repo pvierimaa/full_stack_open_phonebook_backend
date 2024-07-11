589fad89c396ef210df94689a50c2e3c91543a7f
Petri Vierimaa <pvierimaa@gmail.com>
2024-07-11 13:40:25 +0300

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -18,33 +18,33 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
-  response.json(persons)
+    response.json(persons)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-    const currentTime = new Date()
-    Person.countDocuments({})
-      .then(count => {
-        response.send(
-         `<p>Phonebook has info for ${count} people</p>
-          <p>${currentTime}</p>`
+  const currentTime = new Date()
+  Person.countDocuments({})
+    .then(count => {
+      response.send(
+        `<p>Phonebook has info for ${count} people</p>
+         <p>${currentTime}</p>`
       )
-      })
+    })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
         response.status(404).end()
-        }
-      })
-      .catch(error => next(error))  
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -53,21 +53,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))  
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
+    return response.status(400).json({
+      error: 'name missing'
     })
   }
 
   if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
+    return response.status(400).json({
+      error: 'number missing'
     })
   }
 
@@ -81,7 +81,7 @@ app.post('/api/persons', (request, response, next) => {
       response.json(savedPerson)
     })
     .catch(error => next(error))
-})       
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
